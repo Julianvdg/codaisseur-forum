f125420e1a673e3e2170a1812eea40f5e9812e40
nienkebos <nienkebos@live.com>
2016-08-22 22:05:01 +0200

    Add answer to question

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -10,13 +10,13 @@ class AnswersController < ApplicationController
 
   def create
     @question = Question.find(params[:question_id])
-    #@answer.user = current_user
-    @answer = @question.answers.create(params[:answer])
+    @answer = Answer.new(params.require(:answer).permit(:body, :user_id, :question_id))
+
     if @answer.save
-      flash[:notice] = "Thank you for your answer."
-      redirect_to question_path(@question)
+     flash[:notice] = "Thank you for your answer."
+     redirect_to question_path(@question)
     else
-      render :action => 'new'
+     render :action => 'new'
     end
   end
 
