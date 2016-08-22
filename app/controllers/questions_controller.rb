65a9706fcc39819d03362e79a1cbce70ae04da8d
RubenSloote <rubensloote@gmail.com>
2016-08-22 14:24:03 +0200

    edit questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,31 @@
 class QuestionsController < ApplicationController
+
   def index
+    @questions = Questions.order( created_at: :desc )
+    authorize! :read, @questions
   end
 
-  def show
-  end
+  def create
+    question_params = params.require( :question ).permit( :content )
 
-  def new
+     @question = question.new( content: question_params[:content] )
+     @question.user = current_user
+     authorize! :create, @question
   end
 
   def update
+    question_params = params.require( :question ).permit( :content )
+
+    @question = question.update( content: question_params[:content] )
+    @question.user = current_user
+    authorize! :update, @question
   end
+
+  def user
+    @user = User.find( params[:user_id] )
+    authorize! :read, @user
+
+    @question = Question.where( user: @user ).order ( created_at: :desc )
+  end
+
 end
