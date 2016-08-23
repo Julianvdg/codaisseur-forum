f662fba1b3fce51b6a73d4aa2edd303a31c10538
Julian van der Giesen <julianvandergiesen@hotmail.com>
2016-08-23 12:24:26 +0200

    Add user-management page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+    devise_for :users
 
-  root 'questions#index'
-  resources :questions do
-    resources :answers
-  end
+    root 'questions#index'
+    resources :questions do
+        resources :answers
+    end
 
-  get "questions/:id" => "questions#show"
+    get "questions/:id" => "questions#show"
 
+    get "user-management" => "profiles#index"
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
