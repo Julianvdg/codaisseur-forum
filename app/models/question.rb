4a617661faaddda5e3bac5f618140b0cf1eab3a4
RubenSloote <rubensloote@gmail.com>
2016-08-23 13:04:56 +0200

    add search bar

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,9 @@ class Question < ApplicationRecord
   has_many :answers
   belongs_to :topic
 
+  def self.search(search)
+    where("title ILIKE ?", "%#{search}%")
+    where("body ILIKE ?", "%#{search}%")
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -72,7 +72,7 @@ ActiveRecord::Schema.define(version: 20160823085219) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                                 null: false
     t.datetime "updated_at",                                 null: false
-    t.string   "role",                   default: "student", null: false
+    t.string   "role",                   default: "student"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
