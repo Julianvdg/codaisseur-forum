3e1bb9e513f6e690cf3a98c0f86864d53c48cd0f
RubenSloote <rubensloote@gmail.com>
2016-08-24 13:13:19 +0200

    add upload

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,10 @@
-class Profile < ApplicationRecord
+class Profile <  ActiveRecord::Base
   belongs_to :user
 
   def full_name
     self.first_name + " " + self.last_name
   end
+
+  mount_uploader :avatar, ImageUploader
+
 end
