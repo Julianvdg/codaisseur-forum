567a62c9cd39258a81a06d6943051f0ee1384027
Ecthelion3 <matthijs.vandenberg@gmail.com>
2016-08-25 17:12:31 +0200

    add edit course option

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,4 +6,30 @@ class CoursesController < ApplicationController
   def new
     @course = Course.new
   end
+
+  def create
+    @course = Course.new(course_params)
+    @course.save
+    redirect_to courses_path
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course = Course.find(params[:id])
+
+    if @course.update_attributes(course_params)
+      redirect_to courses_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :starting_at, :ending_at)
+  end
 end
