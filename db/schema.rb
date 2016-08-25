4a302e7ff4e38ef0f128a233eb73d9ce8763ca43
Ecthelion3 <matthijs.vandenberg@gmail.com>
2016-08-25 15:28:38 +0200

    remove column student_class from profiles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825123220) do
+ActiveRecord::Schema.define(version: 20160825132637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,10 +42,9 @@ ActiveRecord::Schema.define(version: 20160825123220) do
     t.string   "github"
     t.string   "twitter"
     t.string   "website"
-    t.integer  "student_class"
     t.integer  "user_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "course_id"
     t.index ["course_id"], name: "index_profiles_on_course_id", using: :btree
     t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
