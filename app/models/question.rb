89f0608df354a4a88d738f7e4265f9fa4639c8c7
Ecthelion3 <matthijs.vandenberg@gmail.com>
2016-08-25 13:43:31 +0200

    add topic filter to questions index

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,13 @@
 class Question < ApplicationRecord
+  include Filterable
 
   belongs_to :user
   has_many :answers
   belongs_to :topic
 
+  scope :topic, -> (topic_id) { where topic_id: topic_id }
+
   def self.search(search)
     where("title ILIKE ? ", "%#{search}%")
   end
-
 end
