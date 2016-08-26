e47d6deb1677d69b4671b4acb5d1e2c9502aa1fe
Ecthelion3 <matthijs.vandenberg@gmail.com>
2016-08-26 10:42:22 +0200

    add partial for avatar

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -54,7 +54,6 @@ class ProfilesController < ApplicationController
   end
 
   def edit
-
     @profile = Profile.find(params[:id])
     @user = @profile.user
     authorize! :edit, @profile
@@ -91,7 +90,6 @@ class ProfilesController < ApplicationController
   def sort_direction
     %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
-
 
   def profile_params
     params.require(:profile).permit(:avatar, :first_name, :last_name, :course_id, :bio, :github, :twitter, :website).tap do |person_params|
